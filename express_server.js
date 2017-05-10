0137ed64eb8ecf4dc6c8cf8c7e883a841dae93cb
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-10 19:07:28 +0000

    added delete feature

--- express_server.js
+++ express_server.js
@@ -47,6 +47,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
