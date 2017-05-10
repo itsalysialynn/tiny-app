fefde4dd9f11b5268f53a58d28abc5e377bd40ee
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-10 19:38:38 +0000

    added update feature

--- express_server.js
+++ express_server.js
@@ -47,9 +47,13 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newLongUrl;
   res.redirect("/urls");
 });
 
